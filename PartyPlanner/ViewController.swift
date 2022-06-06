cae5786298b5ec646bb91e5585f2580bf6f4714b
Samuel Pena <inestbht@gmail.com>
2022-06-05 21:44:31 -0500

    Completed - Ch. 5.2. - Exercise 1 - PartyPlanner

--- PartyPlanner/ViewController.swift
+++ PartyPlanner/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var partyItems = ["Potato Chips",
+                      "Tortilla Chips",
+                      "Salsa",
+                      "Chili",
+                      "Punch",
+                      "Sudsy Beverages",
+                      "Brownies",
+                      "Cupcakes",
+                      "Fruit salad",
+                      "Ribs",
+                      "Corn bread",
+                      "Macaroni Salad",
+                      "Sandwich Rolls",
+                      "Roast Beef",
+                      "Ham",
+                      "Cheese",
+                      "Mayo",
+                      "Mustard",
+                      "Hummus",
+                      "Baby carrots",
+                      "Celery",
+                      "Veggie Dip",
+                      "Napkins",
+                      "Plates & Bowls",
+                      "Forks and Knives",
+                      "Cups"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("*** numberOfRowsInSection was just called and there are \(partyItems.count) rows in the tableView")
+        return partyItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = partyItems[indexPath.row]
+        print(">>> Dequeing the table view cell for indexPath.row = \(indexPath.row) where the cell contains item \(partyItems[indexPath.row])")
+        return cell
+    }    
+}
